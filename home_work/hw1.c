2c607fcec1077cae1ddfc3033cbbf3752a443a13
Maksim-altai <veseliu.kloun@yandex.ru>
2024-09-08 14:28:39 +0700

    HW1

---
+++ home_work/hw1.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	
+	printf("Hello world!\n");  // Задание №1 
+	printf("Let`s\n"); // Задание№2
+	printf("   go\n");
+	printf("     to walk\n");
+	printf("   *   \n"); //Задание№3
+	printf("  ***  \n");
+	printf(" ***** \n");
+	printf("*******\n");
+	printf(" HHOHH \n");
+	printf(" ZZZZZ \n");
+}
